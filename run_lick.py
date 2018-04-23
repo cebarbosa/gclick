eb6c5d4b58ac66c1dd57ee7f6fb0091deab67199
cebarbosa <carlos.barbosa@usp.br>
2018-04-23 16:16:49 -0300

    Fixed problem with errors, saving results in table.

--- run_lick.py
+++ run_lick.py
@@ -13,6 +13,7 @@ from __future__ import print_function, division
 import os
 
 import numpy as np
+from astropy.table import Table, hstack
 import matplotlib.pyplot as plt
 
 from lick.lick import Lick
@@ -30,26 +31,36 @@ def mc_schiavon_err(wave, flux, sigma, bands, nsim=10):
     for i in np.arange(nsim):
         lick = Lick(wave, sim[i], bands)
         lick.classic_integration()
-        lick.classic_integration()
         err[i] = lick.classic
     err = np.nanstd(err, axis=0)
     return err
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     # Setting the name of the directories for different users
     dir_ = ps.get_directories()
+    nsim = 10
     ###########################################################################
     # Reading file with definition of Lick indices bands
     table = os.path.join(os.getcwd(), "lick/tables/bands.txt")
     bands = np.loadtxt(table, usecols=(2, 3, 4, 5, 6, 7,))
+    names = np.loadtxt(table, usecols=(0,), dtype="S")
+    colnames = [[_, "e_{}".format(_)] for _ in names]
+    colnames = [item for sublist in colnames for item in sublist]
     ###########################################################################
     # Changing to directory with the data and getting names of the files
-    os.chdir(os.path.join(dir_["data_dir"], "schiavon2005"))
-    filenames = sorted([_ for _ in os.listdir(".") if _.endswith("txt")])
+    os.chdir(os.path.join(dir_["data_dir"], "erros_sch05"))
+    filenames = sorted(os.listdir("."))
+    results = []
     for fname in filenames:
-        wave, spec, var, sn = np.loadtxt(fname, unpack=True)
-        sigma = np.sqrt(var)
+        wave, spec, sigma, sn = np.loadtxt(fname, unpack=True)
         lick = Lick(wave, spec, bands)
         lick.classic_integration()
-        mcerr = mc_schiavon_err(wave, spec, sigma, bands)
-        break
+        indices = lick.classic
+        mcerr = mc_schiavon_err(wave, spec, sigma, bands, nsim=nsim)
+        results.append(np.column_stack([indices, mcerr]).reshape(-1))
+    names = Table([filenames], names=["spec"])
+    results = Table(np.transpose(results).tolist(), names=colnames)
+    results = hstack([names, results])
+    output = os.path.join(dir_["tables_dir"], "lick_sch05_mc{}.fits".format(
+        nsim))
+    results.write(output, overwrite=True)
